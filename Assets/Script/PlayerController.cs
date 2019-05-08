0d6be844c86a39c5c5140bbbdaf1585cc5f5adbe
KurokiMasami <cherry8823@icloud.com>
2019-05-09 00:27:14 +0900

    タイマーの修正、プレイヤーの当たり判定作成。

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // これを入れて置いてください<(_ _)>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
