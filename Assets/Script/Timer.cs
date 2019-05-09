996c6516c10f8e40005d82e86317af6a404ecf59
kuro <kuro.xr5@gmail.com>
2019-05-10 08:57:48 +0900

    コンフリクト起きたため修正 直すほう間違ってたらすみません 本来のマスターに合わせてください

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -5,24 +5,15 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
-<<<<<<< HEAD
-    public float countTime = 60;
-    int seconds;
-=======
+
     public float countTime = 100;
     int seconds;
->>>>>>> 8eaf0340a06933b28cc9d1c998799dfd5f1647c5
 
 	void FixedUpdate () {
         if(countTime >= 0){
             countTime -= Time.deltaTime;
             seconds = (int)countTime;
-<<<<<<< HEAD
-            GetComponent<Text>().text = seconds.ToString();
-        }
-=======
             GetComponent<Text>().text = "Time:   "+ seconds.ToString();
         }
->>>>>>> 8eaf0340a06933b28cc9d1c998799dfd5f1647c5
     }
 }
